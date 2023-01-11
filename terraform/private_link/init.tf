a5a68af0f215aec45d14ec26dd47cbad0239ec3b
Takaaki Yayoi <takaaki.yayoi@databricks.com>
2023-01-11 09:11:41 +0900

    initial commit

---
+++ terraform/private_link/init.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "databricks" {
+  alias    = "mws"
+  host     = "https://accounts.cloud.databricks.com"
+  username = var.databricks_account_username
+  password = var.databricks_account_password
+}
---
+++ terraform/private_link/workspace.tf
@@ -0,0 +1,20 @@
+resource "databricks_mws_private_access_settings" "pas" {
+  provider                     = databricks.mws
+  account_id                   = var.databricks_account_id
+  private_access_settings_name = "Private Access Settings for ${local.prefix}"
+  region                       = var.region
+  public_access_enabled        = true
+}
+
+resource "databricks_mws_workspaces" "this" {
+  provider                   = databricks.mws
+  account_id                 = var.databricks_account_id
+  aws_region                 = var.region
+  workspace_name             = local.prefix
+  credentials_id             = databricks_mws_credentials.this.credentials_id
+  storage_configuration_id   = databricks_mws_storage_configurations.this.storage_configuration_id
+  network_id                 = databricks_mws_networks.this.network_id
+  private_access_settings_id = databricks_mws_private_access_settings.pas.private_access_settings_id
+  pricing_tier               = "ENTERPRISE"
+  depends_on                 = [databricks_mws_networks.this]
+}
